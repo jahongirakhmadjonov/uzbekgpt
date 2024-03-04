e9afb771a327e15045ba1f45ea6256a7176c5733
Jahongir Ahmadjonov <115130799+jahongirakhmadjonov@users.noreply.github.com>
2024-03-04 12:52:34 +0500

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,13 +1,31 @@
 import g4f
 
-def chatbot(zapros, napravleniya):
-    if napravleniya == "медицина":
-        prompt = f"{zapros} - связано с медициной. Ответ на эту {zapros} по точке зрения медицине"
-    elif napravleniya == "IT":
-        prompt = f"{zapros} - связано с IT. Ответ на эту {zapros} по точке зрения IT"
-    response = g4f.ChatCompletion.create(
-        model=g4f.models.gpt_4,
-        messages=[{"role": "user", "content": prompt}],
+content="" ""
+def chatbot(content):
+        response = g4f.ChatCompletion.create(
+            model = g4f.models.gpt_4,
+            # model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": content}],
     )
+        return response
+def savol(savol, mavzu=""):
+        if mavzu=="tibbiyot":
+                content = f"Savolim tibbiyot bilan bog'liq. {savol}ga tibbiyot yo'nalishida javob ber"
+        elif mavzu== "dasturlash":
+                content = f"Savolim dasturlash bilan bog'liq. {savol}ga dasturlash yo'nalishida javob ber " 
+        else:
+                content = f"Ushbu savolga o'zbek tilida javob ber. Savol - {savol}"
+        javob = chatbot(content)
+        return javob
+print(savol("Xatolik"))
+print(32*"*")
+print(savol("Xatolik","tibbiyot"))
+print(32*"*")
+# from g4f.client import Client
 
-    return response
+# client = Client()
+# response = client.images.generate(
+#   model="gemini",
+#   prompt="nazi logo in red flag",
+# )
+# image_url = response.data[0].url
